3694d1fb4c23d548ada62bdfdda14bb99c31ee70
Asimiraq <124700074+Asimiraq@users.noreply.github.com>
2023-02-08 16:12:10 +0300

    Create lol12.js

---
+++ lol12.js
@@ -0,0 +1,154 @@
+
+<style>
+
+input#stealth {
+    margin-left: -17px;
+}
+
+div#tlogins img.fitimg.fl.u-pic {
+    border-radius: 5px 15px 5px 5px;
+}
+
+.border {
+    box-shadow: inset 1px 1px 1px 1px rgba(0,0,0,.075), 0 0 5px rgb(146 146 146 / 64%);
+    border: 1px solid white;
+}
+
+#des3 {
+    text-decoration: dotted;
+    color: #fff;
+    margin-left: 2px!important;
+    display: inline-block;
+    position: absolute;
+    margin-right: 4.4px!important;
+    background: #3b7db7;
+    background-image: url(https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Flag_of_Iraq.svg/220px-Flag_of_Iraq.svg.png);
+    background-blend-mode: luminosity;
+}
+
+a.label.label-primary.fl img.fl {display: none;}
+
+
+
+.fr.borderg, .fr.borderg a {
+    font-size: 10px!important;
+    padding: 0 5px!important;
+    position: relative;
+    color: #ffffff;
+}
+.fr.borderg {
+    border: 0px;
+    margin-top: 0;
+    height: 0;
+
+}
+
+
+label.label.label-primary.mini.fl {
+    padding: 1px;
+    border-radius: 15px!important;
+	margin-top: 1px;
+}
+
+
+.hmsg {background-color: rgb(235, 241, 255)!important;}
+#des1 {margin-bottom: 2px;font-family: 'El Messiri',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}
+
+.ae.fa.label.label-primary.fa-user {
+}
+
+.ae.fa.chats.label.fa-comment.label-primary {
+}
+
+label.ae.fa.chats.label.fa-comment.label-warning {
+    border-radius: 0 0 0 15px;
+}
+
+.ae.fa.label.label-primary.fa-users {
+    
+    
+}
+
+.ae.fa.label.fa-commenting-o.label-warning {
+    
+}
+
+.ae.label.label-primary.fa.fa-gear {
+    color: #ffd900;
+    
+}
+
+#des0 {
+    margin: -134px 66px 2px;
+    border-radius: 10px 15px 1px 1px;
+    position: absolute;
+    width: 49% !important;
+    height: 19px;
+    left: -65px;
+    font-size: 14px !important;
+    color: #fff;
+    padding: 0px 0 !important;
+}
+
+.des2 {
+    background-color: #fff;
+    border-radius: 0;
+    padding: 2px 10px;
+    margin: 3px 8px!important;
+	border-radius: 15px!important;
+}
+
+</style>`).insertBefore("body")
+
+اكواد الوحه هنا 
+=========================================
+	try{
+														
+
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(`<img src="https://awtar-iq.com/sico/1606056328183.jpg" class="fr" style="width:100%;margin-top: 0px;" >
+
+
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:228px 2px 2px;margin-left: 316px!important;" class="fl mini  fa   fa-heart-o btn btn-warning"> زخرفة </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 15px 0px 0px 0px!important;width:70px;margin:199px 2px 2px;margin-left: 316px!important;" class="fl mini  fa   fa-phone btn btn-info"> الأداره </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px 0px 0px 15px!important;width:70px;margin:257px 2px 2px;margin-left: 316px!important;" class="fl mini  fa  fa-paint-brush btn btn-danger"> التطبيق </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px 15px 0px 0px!important;width:70px;margin:199px 2px 2px;" class="fl mini  fa   fa-star btn btn-success"> اشتراك </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;border-radius: 0px!important;width:70px;margin:228px 2px 2px;" class="fl mini  fa   fa-image btn btn-warning"> رمزيات </a>
+<a id="des3" href="https://awtar-iq.com/" style="border: 1px solid white;    border-radius: 0px 0px 15px 0px!important;width:70px;margin:257px 2px 2px;" class="fl mini  fa fa-comment-o  btn btn-info"> شات </a>
+
+`).insertBefore('.nav-tabs');
+
+
+$.getScript("https://rawcdn.githack.com/jfa-w/jfa99.js/4e71fd33268e8d6de18db624b9d12311fba9be79/awtar-iq-jfa0.js");
+
+$(".fa-gear").text("الضبط").css("width","");
+
+$("[data-target=#wall]").attr('title',' متنفس الآقلأم و القلوب ')
+
+
+
+$("[data-target=#settings]").attr('title','     الأعدادات و الضبط')
+$("[data-target=#rooms]").attr('title','   الغرف و الرومـات ')
+$("[data-target=#chats]").attr('title','      المحادثات الخاصه  ')
+$("[data-target=#users]").attr('title','  ضيـوفنـا المتـواجدين  ')
+
+
+
+$(`<div><marquee direction="right" width="100%" id="des0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> اهلا وسهلا بكم في شات <font color="#357524" class="des2"> نجمة الحب للجوال </font>افضل سوبر لهذا الاسبوع<font class="des2" color="#800000"> قريبا </font>افضل زائر لهذا الاسبوع<font class="des2" color="#ff9900"> قريبا </font>افضل زائرة لهذا الاسبوع<font class="des2" color="#ff00ff"> قريبا 	 </font>مبدع الحائط لهذا الاسبوع<font class="des2" color="#5CAD9A"> قريبا </font>مبدعه الحائط لهذا الاسبوع<font class="des2" color="#800000"> َقريبا </font>مع تمنياتنا للجميع باطيب الاوقات </marquee></div>`).insertBefore('.lonline.light.break');
+
+(document.getElementById("tbox").placeholder = "(  عراقنا -واحد )");
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
+<center><iframe src="https://mixlr.com/%D9%86%D9%88%D8%B1-%D8%B9%D9%85%D8%A7%D9%86/embed/?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
+<center></center></center>
+</div>`);
+$('.fr.borderg a').text('دوك موبايل')
+$("a.label.label-primary.fl > img.fl").replaceWith('<img src="" class="fl" style="margin-right: 0px; width: 0px;">');$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://jfa-w.net/sico/1580237645774.png" width="70%" style="">`)
+
+}
+														catch (e){
+															console.log(e)
+														}
+
+													
